4169f39ec90ebca7f9bd2846112b0031c659cd1f
Matthew Chambers <66091821+matthewlchambers@users.noreply.github.com>
2025-07-04 21:57:42 +0000

    Pass through math functions

--- unit_df/data_frame.py
+++ unit_df/data_frame.py
@@ -13,7 +13,52 @@ class UnitDataFrame:
         return self.data.__repr__()
 
     def __add__(self, other):
-        return self.data.__add__(other)
+        return UnitDataFrame(self.data.__add__(other), units=self.units)
+
+    def __sub__(self, other):
+        return UnitDataFrame(self.data.__sub__(other), units=self.units)
+
+    def __mul__(self, other):
+        return UnitDataFrame(self.data.__mul__(other), units=self.units)
+
+    def __matmul__(self, other):
+        return UnitDataFrame(self.data.__matmul__(other), units=self.units)
+
+    def __truediv__(self, other):
+        return UnitDataFrame(self.data.__truediv__(other), units=self.units)
+
+    def __floordiv__(self, other):
+        return UnitDataFrame(self.data.__floordiv__(other), units=self.units)
+
+    def __mod__(self, other):
+        return UnitDataFrame(self.data.__mod__(other), units=self.units)
+
+    def __divmod__(self, other):
+        return UnitDataFrame(self.data.__divmod__(other), units=self.units)
+
+    def __radd__(self, other):
+        return UnitDataFrame(self.data.__add__(other), units=self.units)
+
+    def __rsub__(self, other):
+        return UnitDataFrame(self.data.__sub__(other), units=self.units)
+
+    def __rmul__(self, other):
+        return UnitDataFrame(self.data.__mul__(other), units=self.units)
+
+    def __rmatmul__(self, other):
+        return UnitDataFrame(self.data.__matmul__(other), units=self.units)
+
+    def __rtruediv__(self, other):
+        return UnitDataFrame(self.data.__truediv__(other), units=self.units)
+
+    def __rfloordiv__(self, other):
+        return UnitDataFrame(self.data.__floordiv__(other), units=self.units)
+
+    def __rmod__(self, other):
+        return UnitDataFrame(self.data.__mod__(other), units=self.units)
+
+    def __rdivmod__(self, other):
+        return UnitDataFrame(self.data.__divmod__(other), units=self.units)
 
     def __getattr__(self, name: str):
         print(name)
