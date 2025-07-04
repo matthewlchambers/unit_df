559cc3a834c44c8d02bcd1cb821ca3f195c6088e
Matthew Chambers <66091821+matthewlchambers@users.noreply.github.com>
2025-07-04 21:01:23 +0000

    + works now

--- unit_df/data_frame.py
+++ unit_df/data_frame.py
@@ -5,15 +5,25 @@ class UnitDataFrame:
     data: pd.DataFrame
     units: Quality | dict[str, Quality]
 
-    def __init__(self, data, units):
+    def __init__(self, data, units=None):
         self.data = pd.DataFrame(data)
         self.units = units
 
+    def __repr__(self):
+        return self.data.__repr__()
+
+    def __add__(self, other):
+        return self.data.__add__(other)
+
     def __getattr__(self, name: str):
-        def wrapped_attr(*args, **kwargs):
-            data = getattr(self.data, name)(*args, **kwargs)
-            return UnitDataFrame(data, self.units)
+        print(name)
         match (data_attr := getattr(self.data, name)):
             case pd.DataFrame():
                 return UnitDataFrame(data_attr, self.units)
-            case callable(data_attr)
+            case f if callable(data_attr):
+                def wrapped_attr(*args, **kwargs):
+                    data = f(*args, **kwargs)
+                    return UnitDataFrame(data, self.units)
+                return wrapped_attr
+            case _:
+                return data_attr
